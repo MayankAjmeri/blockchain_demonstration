91c56c06a3577663a894a0cd1262b23f1f9d2591
Ashutosh Singh <thecodepapaya@gmail.com>
2021-10-24 14:58:04 +0530

    Add Distributed peers

--- src/Components/Distributed/Distributed.jsx
+++ src/Components/Distributed/Distributed.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import Miner from "../Miner/Miner";
+import DistributedMiner from "../Miner/DistributedMiner";
 import "./Distributed.scss";
 import crypto from "crypto";
 
 const Distributed = () => {
   const [blocks, setBlocks] = useState([
-    {
+    [{
       id: 0,
       block: 1,
       nonce: 11316,
@@ -44,11 +44,91 @@ const Distributed = () => {
       data: null,
       prev: "0000000000000000000000000000000000000000000000000000000000000000",
       hash: null,
+    },], [{
+      id: 0,
+      block: 1,
+      nonce: 11316,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
     },
-  ]);
+    {
+      id: 1,
+      block: 2,
+      nonce: 35230,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 2,
+      block: 3,
+      nonce: 12937,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 3,
+      block: 4,
+      nonce: 35990,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 4,
+      block: 5,
+      nonce: 56265,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },], [{
+      id: 0,
+      block: 1,
+      nonce: 11316,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 1,
+      block: 2,
+      nonce: 35230,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 2,
+      block: 3,
+      nonce: 12937,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 3,
+      block: 4,
+      nonce: 35990,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },
+    {
+      id: 4,
+      block: 5,
+      nonce: 56265,
+      data: null,
+      prev: "0000000000000000000000000000000000000000000000000000000000000000",
+      hash: null,
+    },]
+  ])
 
-  const updateHash = (tempBlock) => {
-    tempBlock.forEach((ele, index, tempBlock) => {
+
+  const updateHash = (tempBlock, peer) => {
+
+    tempBlock[peer].forEach((ele, index, tempBlock) => {
       const prev = index > 0 ? tempBlock[index - 1].hash : ele.prev;
       const hash = crypto
         .createHash("sha256")
@@ -63,60 +143,63 @@ const Distributed = () => {
   };
 
   useEffect(() => {
-    setBlocks([...updateHash(blocks)]);
+    setBlocks([...updateHash(blocks, 0)]);
+    setBlocks([...updateHash(blocks, 1)]);
+    setBlocks([...updateHash(blocks, 2)]);
   }, []);
 
-  const handleBlockChange = (index, value) => {
+  const handleBlockChange = (peer, index, value) => {
     const temp = blocks;
-    // console.log("index, value", index, value);
+    // 
 
-    temp[index].block = value;
-    // console.log([...updateHash(temp)]);
-    setBlocks([...updateHash(temp)]);
+    temp[peer][index].block = value;
+    // 
+    setBlocks([...updateHash(temp, peer)]);
   };
 
-  const handleNonceChange = (index, value) => {
+  const handleNonceChange = (peer, index, value) => {
     const temp = blocks;
-    console.log(temp[index]);
-    temp[index].nonce = value;
-    setBlocks([...updateHash(temp)]);
+
+    temp[peer][index].nonce = value;
+    setBlocks([...updateHash(temp, peer)]);
   };
 
-  const handleDataChange = (index, value) => {
+  const handleDataChange = (peer, index, value) => {
     const temp = blocks;
-    temp[index].data = value;
-    setBlocks([...updateHash(temp)]);
+    temp[peer][index].data = value;
+    setBlocks([...updateHash(temp, peer)]);
   };
 
-  const clickHandler = (index) => {
+  const clickHandler = (peer, index) => {
     const temp = blocks;
-    console.log(index);
+    // 
+    // 
     let nonce = 1;
     const hash = crypto.createHash("sha256");
     let is_req_hash = false;
-    console.log(temp[index]);
+
     let hash_value;
     while (!is_req_hash) {
       hash_value = hash
-        .update(temp[index].block + nonce + temp[index].data + temp[index].prev)
+        .update(temp[peer][index].block + nonce + temp[peer][index].data + temp[peer][index].prev)
         .digest("hex");
       if (hash_value.substr(0, 4) === "0000") {
         is_req_hash = true;
       }
       nonce++;
     }
-    console.log(hash_value);
-    temp[index].hash = hash_value;
-    temp[index].nonce = nonce;
+
+    temp[peer][index].hash = hash_value;
+    temp[peer][index].nonce = nonce;
     index++;
-    for (; index < temp.length; index++) {
-      const prev = index > 0 ? temp[index - 1].hash : temp[index].prev;
+    for (; index < temp[peer].length; index++) {
+      const prev = index > 0 ? temp[peer][index - 1].hash : temp[peer][index].prev;
       const hash = crypto
         .createHash("sha256")
-        .update(temp[index].block + temp[index].nonce + temp[index].data + prev)
+        .update(temp[peer][index].block + temp[peer][index].nonce + temp[peer][index].data + prev)
         .digest("hex");
-      temp[index].prev = prev;
-      temp[index].hash = hash;
+      temp[peer][index].prev = prev;
+      temp[peer][index].hash = hash;
     }
     setBlocks([...temp]);
   };
@@ -125,11 +208,12 @@ const Distributed = () => {
       <h1>Distributed Blockchain </h1>
       <h2>Peer A</h2>
       <div className="peers">
-        {blocks.map((block) => {
+        {blocks[0].map((block) => {
           return (
-            <Miner
+            <DistributedMiner
               key={block.id}
               blockchain={block}
+              peer={0}
               handleBlockChange={handleBlockChange}
               handleDataChange={handleDataChange}
               handleNonceChange={handleNonceChange}
@@ -140,10 +224,11 @@ const Distributed = () => {
       </div>
       <h2>Peer B</h2>
       <div className="peers">
-        {blocks.map((block) => {
+        {blocks[1].map((block) => {
           return (
-            <Miner
+            <DistributedMiner
               key={block.id}
+              peer={1}
               blockchain={block}
               handleBlockChange={handleBlockChange}
               handleDataChange={handleDataChange}
@@ -155,10 +240,11 @@ const Distributed = () => {
       </div>
       <h2>Peer C</h2>
       <div className="peers">
-        {blocks.map((block) => {
+        {blocks[2].map((block) => {
           return (
-            <Miner
+            <DistributedMiner
               key={block.id}
+              peer={2}
               blockchain={block}
               handleBlockChange={handleBlockChange}
               handleDataChange={handleDataChange}
---
+++ src/Components/Miner/DistributedMiner.jsx
@@ -0,0 +1,89 @@
+import "./Miner.scss";
+import { useState } from "react";
+
+const DistributedMiner = ({
+    peer,
+    blockchain,
+    handleBlockChange,
+    handleDataChange,
+    handleNonceChange,
+    clickHandler,
+}) => {
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <div className="miner">
+            <div className="dataform">
+                <h4>Block: </h4>
+                <input
+                    type="number"
+                    value={blockchain.block}
+                    onChange={(e) => handleBlockChange(peer, blockchain.id, e.target.value)}
+                    className="miner_Data_Input"
+                />
+            </div>
+            <div className="dataform">
+                <h4>Nonce: </h4>
+                <input
+                    type="number"
+                    value={blockchain.nonce}
+                    onChange={(e) => handleNonceChange(peer, blockchain.id, e.target.value)}
+                    className="miner_Data_Input"
+                />
+            </div>
+            <div className="dataform">
+                <h4>Data: </h4>
+                <textarea
+                    value={blockchain.data}
+                    onChange={(e) => handleDataChange(peer, blockchain.id, e.target.value)}
+                    rows={10}
+                    className="miner_Data_Input"
+                />
+            </div>
+            <div className="dataform">
+                <h4>Prev: </h4>
+                <textarea
+                    disabled
+                    rows={3}
+                    className="miner_Data_Input"
+                    placeholder={blockchain.prev}
+                />
+            </div>
+            <div className="dataform">
+                <h4>Hash: </h4>
+                <textarea
+                    disabled
+                    rows={3}
+                    className="miner_Data_Input"
+                    placeholder={blockchain.hash}
+                />
+            </div>
+            <button
+                className="mine"
+                onClick={(e) => {
+                    setLoading(true);
+                    setTimeout(() => {
+                        clickHandler(peer, blockchain.id);
+                        setLoading(false);
+                    }, 500);
+                }}
+                disabled={loading}
+            >
+                {!loading ? (
+                    <p style={{ margin: "0px" }}>Mine</p>
+                ) : (
+                    <p style={{ margin: "0px", justifyContent: "center" }}>
+                        {" "}
+                        Mining
+                        <i
+                            className="fa fa-spinner fa-spin"
+                            style={{ margin: "0 5px", fontSize: "24px" }}
+                        />
+                    </p>
+                )}
+            </button>
+        </div>
+    );
+};
+
+export default DistributedMiner;
